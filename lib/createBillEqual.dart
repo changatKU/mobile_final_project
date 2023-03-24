bce7d7af8b0c168b905fb07fba0eff92e8add0e3
Coolfreewerx <nattapon.mee@ku.th>
2023-03-24 22:05:34 +0700

    edit text, add function not completed

--- lib/createBillEqual.dart
+++ lib/createBillEqual.dart
@@ -14,30 +14,29 @@ class CreateBillEqualScreen extends StatefulWidget {
 }
 
 class _CreateBillEqualScreenState extends State<CreateBillEqualScreen> {
-  TextField createBillField(
-      String hint_text, TextInputType textInputType) {
-    return TextField(
+  TextField createBillField(String hint_text, TextInputType textInputType) 
+  {
+      return TextField(
       decoration: InputDecoration(
         labelText: hint_text,
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(40.0)),
         hintText: hint_text,
-        hintStyle: TextStyle(fontSize: 10, height: 2.0),
-        contentPadding: EdgeInsets.only(left: 80),
+        hintStyle: TextStyle(
+          fontSize: 10, 
+          height: 1.5),
+        contentPadding: EdgeInsets.only(left: 70),
         //textAlign: TextAlign.center
       ),
       keyboardType: textInputType,
     );
   }
 
+
   @override
   Widget build(BuildContext context) {
     // List<Widget> widget=<Widget>[];
 
     return Scaffold (
-      // appBar: AppBar (
-      //   title: Text ('Create Bill',),
-      // centerTitle: true,
-    // ),
       body: SafeArea(
         child: SingleChildScrollView(
           child:
@@ -101,7 +100,17 @@ class _CreateBillEqualScreenState extends State<CreateBillEqualScreen> {
 
         Container(
           margin: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-          child: createBillField('Type Topic', TextInputType.text)
+          child: TextField(
+      decoration: InputDecoration(
+        labelText: 'Type Topic',
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(40.0)),
+        hintText: 'Type Topic',
+        hintStyle: TextStyle(fontSize: 10, height: 2.0),
+        contentPadding: EdgeInsets.only(left: 140),
+        //textAlign: TextAlign.center
+      ),
+      keyboardType: TextInputType.text,
+    )
         ),
 
         Container(
@@ -132,48 +141,52 @@ class _CreateBillEqualScreenState extends State<CreateBillEqualScreen> {
 
         Container(
           margin: EdgeInsets.only(top: 20.0, left: 20.0),
-          child: Text('Member', style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,          
-          )),
+          child: Row(
+            children: [
+              Row(children: [
+                Text('Member', style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),),
+                Text(' (Seach by Phone Number)', style: TextStyle(
+                  fontSize: 12,
+                  height: 1.5,
+                  fontWeight: FontWeight.w600
+                )),
+              ],)
+            ],
+          ),
         ),
 
-        // Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children:
-        // [],),
+
 
         Container(
           margin: EdgeInsets.only(top: 20.0, left: 20.0, right: 70.0),
-          child:
-            // Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children:
-            // [
-              createBillField('Type Phone Number', TextInputType.text), 
-            // ],),
+          child: Row(
+            children: [
+              Row(children: [
+                Text('Type Phone Number'),
+                // Builder(builder: addMember)
+              ],)
+            ],
           ),
-        
-        Container(
-          child: ElevatedButton (
-            onPressed: () {
-              Navigator.push (
-              context, MaterialPageRoute(
-                builder: (context) => const RegisterScreen() // change route
-                ),);
-              }, 
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children:
-            [
-              Icon(
-                Icons.arrow_back_outlined,
-                size: 15.0),
-                Padding(padding: EdgeInsets.only(left: 6.0)),
-                Text('Add'),
-              ],),),
-          
-          ),
-
-
-         ],), 
+          // createBillField('Type Phone Number', TextInputType.text),
         ),
-    ),
-    );
+
+            Container(child: ElevatedButton (
+              onPressed: () {
+                Navigator.push (
+                context, MaterialPageRoute(
+                  builder: (context) => const RegisterScreen() // add list view 
+                  ),);
+              },
+              child: Text('Add'),
+              ),),
+            
+        ],),),
+        
+      ),
+      ); 
   }
 }
 
